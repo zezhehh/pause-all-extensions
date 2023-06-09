020a6bf40dbfc732891facf970271a5cf8cfce4e
zezhehh <zezhehh@outlook.com>
2023-06-09 19:58:25 +0200

    avoid unnecessary rerender

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Button from "@mui/material/Button";
 import ButtonGroup from '@mui/material/ButtonGroup';
+import DeleteIcon from '@mui/icons-material/Delete';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import CheckIcon from '@mui/icons-material/Check';
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert, { AlertProps, AlertColor } from '@mui/material/Alert';
 import { getExtInfoKey, STORAGE_PREFIX, getGroupInfoKey, groupNumKey, advancedKey } from "./constants";
@@ -21,7 +24,7 @@ const App = () => {
   const extStatus = useExtStatus();
   const storage = useRef<ExtStorage>(new ExtStorage('sync'));
   const pauseController = useRef<PauseController>(new PauseController(storage.current))
-  const [groupNum, groups, ungrouped] = useGroups(extStatus, storage.current);
+  const [groupNum, groups, groupStatus, ungrouped] = useGroups(extStatus, storage.current);
 
   const [paused, setPaused] = useState(false);
   const [open, setOpen] = useState(false);
@@ -148,24 +151,24 @@ const App = () => {
   )
 
   const groupBar = (groupID: string) => {
-    const extPaused = groups[groupID].paused
+    const extPaused = groupStatus[groupID]
     return (
-      <ButtonGroup variant="outlined">
+      <ButtonGroup size="small" fullWidth>
         <Button onClick={() => {
           if (extPaused) {
-            pauseController.current.unpause(...groups[groupID].exts)
-            storage.current.set({ [groupID]: { ...groups[groupID], paused: false } })
+            pauseController.current.unpause(...groups[groupID])
+            storage.current.set({ [groupID]: { exts: groups[groupID], paused: false } })
           }
           else {
-            pauseController.current.pause(...groups[groupID].exts)
-            storage.current.set({ [groupID]: { ...groups[groupID], paused: true } })
+            pauseController.current.pause(...groups[groupID])
+            storage.current.set({ [groupID]: { exts: groups[groupID], paused: true } })
           }
-        }}>{extPaused ? 'Resume' : 'Pause'}</Button>
+        }} style={{ width: '85%' }}>{extPaused ? 'Resume Group' : 'Pause Group'}</Button>
         <Button onClick={() => {
           storage.current.remove(groupID);
           storage.current.set({ [groupNumKey]: groupNum - 1 })
           storage.current.set({ [groupNumKey]: groupNum + 1 })
-        }}>Remove</Button>
+        }} style={{ borderRadius: 0, width: '15%' }}><DeleteIcon /></Button>
       </ButtonGroup>
 
     )
@@ -173,24 +176,33 @@ const App = () => {
 
   return (
     <div className="App">
+      <div className='toggle-button'>
+        <Button variant="contained" onClick={togglePause} fullWidth style={{marginBottom: 5}}>
+          {paused ? "Resume" : (advanced ? "Pause All" : "Pause")}
+        </Button>
+        {advanced ?
+          <ButtonGroup size="small" fullWidth>
+            <Button onClick={toggleAdvanced} style={{ width: '85%' }}>
+              Switch to Simple
+            </Button>
+            <Button onClick={toggleGrouping} style={{ borderRadius: 0, width: '15%' }}>
+              {grouping ? <CheckIcon /> : <LibraryAddIcon />}
+            </Button>
+          </ButtonGroup>
+          :
+          <Button variant="outlined" size="small" fullWidth onClick={toggleAdvanced}>
+            Switch to Advanced
+          </Button>
+        }
+      </div>
 
-      <Button variant="contained" onClick={togglePause} className='toggle-button'>
-        {paused ? "Resume" : (advanced ? "Pause All" : "Pause")}
-      </Button>
-      <Button variant="contained" onClick={toggleAdvanced} className='toggle-button'>
-        {advanced ? "Advanced Version" : "Simple Version"}
-      </Button>
-      {advanced && <Button variant="contained" onClick={toggleGrouping} className='toggle-button'>
-        {grouping ? "Confirm" : "Group extensions"}
-      </Button>}
       <div className="ext-info">
-
         {advanced &&
           Object.keys(groups).map((groupID: string) => {
             const group = groups[groupID];
             return <div key={groupID} className='grouped'>
               {groupBar(groupID)}
-              {group.exts.map((extID: string) => {
+              {group.map((extID: string) => {
                 const extEnabled = extStatus[extID].enabled;
                 const extShortName = extStatus[extID].shortName;
                 return renderExt(extID, extEnabled, extShortName)
