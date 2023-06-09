f70ed26beeb4125a8063bbd6f828f08385ec179d
kiki0805 <kiki_hzz@163.com>
2023-06-09 20:33:24 +0200

    format

--- src/Storage.tsx
+++ src/Storage.tsx
@@ -1,36 +1,36 @@
 // Define a custom string type
-type ExtStorageMode = 'local' | 'sync';
+type ExtStorageMode = "local" | "sync";
 
 class ExtStorage {
-    mode: ExtStorageMode;
-    storage: chrome.storage.StorageArea;
+  mode: ExtStorageMode;
+  storage: chrome.storage.StorageArea;
 
-    constructor(mode: ExtStorageMode) {
-        this.mode = mode;
+  constructor(mode: ExtStorageMode) {
+    this.mode = mode;
 
-        if (mode === 'local') {
-            this.storage = chrome.storage.local;
-        } else {
-            this.storage = chrome.storage.sync;
-        }
+    if (mode === "local") {
+      this.storage = chrome.storage.local;
+    } else {
+      this.storage = chrome.storage.sync;
     }
+  }
 
-    async get(...keys: string[]) {
-        const result = await this.storage.get(keys);
-        return result;
-    }
+  async get(...keys: string[]) {
+    const result = await this.storage.get(keys);
+    return result;
+  }
 
-    async set(items: {[key: string]: any}) {
-        await this.storage.set(items);
-    }
+  async set(items: { [key: string]: any }) {
+    await this.storage.set(items);
+  }
 
-    async clear() {
-        await this.storage.clear();
-    }
+  async clear() {
+    await this.storage.clear();
+  }
 
-    async remove(...keys: string[]) {
-        await this.storage.remove(keys);
-    }
+  async remove(...keys: string[]) {
+    await this.storage.remove(keys);
+  }
 }
 
 export default ExtStorage;
